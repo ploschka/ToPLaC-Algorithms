16e31b0d3b31ac2e5fbadf8edab853becb5ea737
Limuran <strelgl2@gmail.com>
2022-12-08 00:31:25 +1000

    Дополнены комментарии. Добавлены вспомогательные методы

--- grammar.py
+++ grammar.py
@@ -1,6 +1,26 @@
 class Grammar:
     def __init__(self, N: set[str], T: set[str], P: dict[str, list[str]], S: str):
-        self.N = N # нетерминалы
-        self.T = T # терминалы
-        self.P = P # правила
-        self.S = S # аксиома (начальный символ грамматики)
+        self.N = N  # Нетерминалы. Например, {"E", "T", "F"}
+        self.T = T  # Терминалы. Например, {"a", "(", ")"}
+        self.P = P  # Правила. Например, { "E": ["E+T", "T"], "T": ["T*F", "F"], "F": ["(E)", "a"] }
+        self.S = S  # Аксиома (начальный символ грамматики). Например, "E"
+
+    def __str__(self):
+        """Метод для красивого вывода грамматики с помощью print()"""
+        rules = ""
+        for non_term in self.P.keys():
+            rules += f"\n\t\t{non_term} -> " + \
+                     " | ".join(self.P[non_term])
+        return f"""{{
+    Нетерминалы: {self.N}
+    Терминалы: {self.T}
+    Правила: {rules}
+    Аксиома: {self.S}
+}}"""
+
+    def __eq__(self, other):
+        """Оператор равенства грамматик"""
+        return self.N == other.N and \
+               self.T == other.T and \
+               self.P == other.P and \
+               self.S == other.S
